9017c2260a18c9e7968d29298406835ca3222c91
celiatorres <76687776+celiatorres@users.noreply.github.com>
2021-01-31 18:49:23 +0100

    cabecera

--- funciones.php
+++ funciones.php
@@ -1,7 +1,7 @@
 <?php
 
 function conectar(){
-	$con =  mysqli_connect('localhost', 'root', '', 'FMCIA');
+	$con =  mysqli_connect('localhost', 'root', '', 'fmcia');
 	
 	if ($con) $con->set_charset( 'UTF8');
 	if ($con->connect_error) {
@@ -24,25 +24,17 @@ function pie(){
 	<?php
 }
 
-
-
-
-
-
-
-
-
 function encabezado($administrador){
 	if($administrador==2){
         ?>
         	<header>
-		<a href="index.html">
+		<a href="index.php">
 			<img class="icono" src="imagenes/Iconos/fm_arriba.png" alt="Fm-cia"/>
 		</a>
 
 		<nav class="navegacion">
 			<ul class="menu">
-				<li><a href="biologia.html">Biología</a></li>
+				<li><a href="biologia.php">Biología</a></li>
 
 				<?php
 				/*foreach categoria(acceso a table)
@@ -59,54 +51,54 @@ function encabezado($administrador){
 				foreach categoria (acceso a tabla) x4*/
 				?>
 				<li>|</li>
-				<li><a href="fisica.html">Física</a></li>
+				<li><a href="fisica.php">Física</a></li>
 				<li>|</li>
-				<li><a href="quimica.html">Química</a></li>
+				<li><a href="quimica.php">Química</a></li>
 				<li>|</li>
-				<li><a href="tecnologia.html">Tecnología</a>
+				<li><a href="tecnologia.php">Tecnología</a>
 					<ul class="submenu">
-						<li><a href="informatica.html">Informática</a></li>
-						<li><a href="robotica.html">Robótica</a></li>
-						<li><a href="biotecnologia.html">Biotecnología</a></li>
+						<li><a href="informatica.php">Informática</a></li>
+						<li><a href="robotica.php">Robótica</a></li>
+						<li><a href="biotecnologia.php">Biotecnología</a></li>
 					</ul>
 			</ul>
 		</nav>
 
 		<!--Registro y Crear cuenta con los enlaces-->
 		<article class="p1">
-			<h2 class="no"><a href="registro.html">Crear cuenta</a>  |  <a href="inicio_sesion.html">Iniciar sesión</a></h2>		</article>
+			<h2 class="no"><a href="registro.php">Crear cuenta</a>  |  <a href="inicio_sesion.php">Iniciar sesión</a></h2>		</article>
     </header>
         <?php
     }
     if($administrador==0){
         ?>
         <header>
-		<a href="index_registrado.html">
+		<a href="index_registrado.php">
 			<img class="icono" src="imagenes/Iconos/fm_arriba.png" alt="Fm-cia"/>
 		</a>
 
 		<nav class="navegacion">
 			<ul class="menu">
-				<li><a href="biologia_registrado.html">Biología</a></li>
+				<li><a href="biologia_registrado.php">Biología</a></li>
 				<li>|</li>
-				<li><a href="fisica_registrado.html">Física</a></li>
+				<li><a href="fisica_registrado.php">Física</a></li>
 				<li>|</li>
-				<li><a href="quimica_registrado.html">Química</a></li>
+				<li><a href="quimica_registrado.php">Química</a></li>
 				<li>|</li>
-				<li><a href="tecnologia_registrado.html">Tecnología</a>
+				<li><a href="tecnologia_registrado.php">Tecnología</a>
 					<ul class="submenu">
-						<li><a href="informatica_registrado.html">Informática</a></li>
-						<li><a href="robotica_registrado.html">Robótica</a></li>
-						<li><a href="biotecnologia_registrado.html">Biotecnología</a></li>
+						<li><a href="informatica_registrado.php">Informática</a></li>
+						<li><a href="robotica_registrado.php">Robótica</a></li>
+						<li><a href="biotecnologia_registrado.php">Biotecnología</a></li>
                     </ul>
                 <li>|</li>
-				<li><a href="enviar_articulo.html">Enviar artículo</a></li>
+				<li><a href="enviar_articulo.php">Enviar artículo</a></li>
 			</ul>
 		</nav>
 
 		<!--Mi perfil y cerrar sesion lleva a la pagina inicial en vista no registrado-->
 		<article class="p1">
-			<h2 class="no"><a href="mi_perfil.html">Mi perfil</a>  |  <a href="../index.html">Cerrar sesión</a></h2>
+			<h2 class="no"><a href="mi_perfil.php">Mi perfil</a>  |  <a href="../index.php">Cerrar sesión</a></h2>
         </article>
     </header>
     <?php
@@ -114,37 +106,37 @@ function encabezado($administrador){
     if($administrador==1){
         ?>
         <header>
-		<a href="index_administrador.html">
+		<a href="index_administrador.php">
 			<img class="icono" src="imagenes/Iconos/fm_arriba.png" alt="Fm-cia"/>
 		</a>
 
         <nav class="navegacion">
 
 			<ul class="menu">
-				<li><a href="biologia_administrador.html">Biología</a></li>
+				<li><a href="biologia_administrador.php">Biología</a></li>
 				<li>|</li>
-				<li><a href="fisica_administrador.html">Física</a></li>
+				<li><a href="fisica_administrador.php">Física</a></li>
 				<li>|</li>
-				<li><a href="quimica_administrador.html">Química</a></li>
+				<li><a href="quimica_administrador.php">Química</a></li>
 				<li>|</li>
-				<li><a href="tecnologia_administrador.html">Tecnología</a>
+				<li><a href="tecnologia_administrador.php">Tecnología</a>
 					<ul class="submenu">
-						<li><a href="informatica_administrador.html">Informática</a></li>
-						<li><a href="robotica_administrador.html">Robótica</a></li>
-						<li><a href="biotecnologia_administrador.html">Biotecnología</a></li>
+						<li><a href="informatica_administrador.php">Informática</a></li>
+						<li><a href="robotica_administrador.php">Robótica</a></li>
+						<li><a href="biotecnologia_administrador.php">Biotecnología</a></li>
                     </ul>
                 <li>|</li>
-				<li><a href="publicar_articulo.html">Publicar artículo</a></li>
+				<li><a href="publicar_articulo.php">Publicar artículo</a></li>
 				<li>|</li>
-                <li><a href="eliminar_usuario.html">Eliminar usuario</a></li>
+                <li><a href="eliminar_usuario.php">Eliminar usuario</a></li>
                 <li>|</li>
-				<li><a href="modificar_categorias.html">Modificar Categorías</a></li>
+				<li><a href="modificar_categorias.php">Modificar Categorías</a></li>
 			</ul>
 		</nav>
 
 		<!--Mi perfil y cerrar sesion lleva a la pagina inicial en vista no registrado-->
 		<article class="p1">
-			<h2 class="no"><a href="mi_perfil.html">Mi perfil</a>  |  <a href="../index.html">Cerrar sesión</a></h2>
+			<h2 class="no"><a href="mi_perfil.php">Mi perfil</a>  |  <a href="../index.php">Cerrar sesión</a></h2>
         </article>
     </header>
     <?php
